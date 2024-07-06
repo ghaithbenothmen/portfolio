d57e880b753244ee5899d2272966cc1afa1c3c7b
ghaithbenothmen <ghaithbenothmen8@gmail.com>
2024-07-07 00:32:20 +0100

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
 const App = () => {
   return (
     
-    <HashRouter>
+    <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -23,7 +23,7 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
