92e15e27e89d238d3192bf336f932ca6acc1cdf9
ghaithbenothmen <ghaithbenothmen8@gmail.com>
2024-07-06 18:27:37 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio/', // Ensure this matches your GitHub repository name
   plugins: [react()],
-  base: '/portfolio/', // Add this line
 });
